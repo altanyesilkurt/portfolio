a049bbf8a3870b767d7d58a9b680235d6ef6f58c
Altan Yeşilkurt <altan.yesilkurt@litum.com>
2021-04-12 09:25:43 +0300

    created AboutMe component

--- src/App.js
+++ src/App.js
@@ -3,32 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Particles from 'react-particles-js';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <>
       <Particles
-      className="particles-canvas"
-      params={{
-        particles:{
-          number:{
-            value:35,
-            density:{
-              enable:true,
-              value_area:900
-            }
-          },
-          shape:{
-            type:'square',
-            stroke:{
-              width:6,
-              color:'#f9ab00'
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 35,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: 'square',
+              stroke: {
+                width: 6,
+                color: '#f9ab00'
+              }
             }
           }
-        }
-      }} />
+        }} />
       <Navbar />
       <Header />
+      <AboutMe />
     </>
   );
 }
