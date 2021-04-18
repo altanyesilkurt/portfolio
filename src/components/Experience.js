53e2443e935171038268717fcc5d33c6012bd769
Altan Yeşilkurt <altan.yesilkurt@litum.com>
2021-04-18 22:23:41 +0300

    updated experience content

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -18,7 +18,7 @@ const Experience = () => {
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2019-2020</h3>
+                        <h3>2018-2019</h3>
                         <p>I worked as a software developer at IPERA Technology Solution company <br />I develop the Frontend layers using HTML5,CSS3 and Vue JS.
                         <br />I developed RestAPI using Java Spring Boot with good quality of code<br />I created running Unit test and worked on integration testing
                         <br />I worked on agile scrum methodology and involving in stand-up meetings to monitor progress</p>
@@ -28,7 +28,7 @@ const Experience = () => {
                 <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2020-2021</h3>
+                        <h3>2019-2020</h3>
                         <p>I worked as a software developer at CFM Cooling and Automation company <br />I developed the Frontend layers using HTML5,CSS3, Angular 8,Angular Material, DevExtreme Material.
                         <br />I experienced Docker and its tools<br />I created running unit test and worked on integration testing using with karma, jasmine (over 1000+) , Selenium
                         <br />I worked on agile scrum methodology and involving in stand-up meetings to monitor progress(JIRA)</p>
@@ -38,7 +38,7 @@ const Experience = () => {
                     {/* - */}
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2021- ∞</h3>
+                        <h3>2020-2021(∞)</h3>
                         <p>I worked as a software developer at Litum Technology company <br />I developed the Frontend layers using HTML5,CSS3, Angular 7,Angular Material, DevExtreme Material.
                         <br />I exprerienced RabbitMQ and Redis.<br />I created running unit test and worked on integration testing using with karma, jasmine (over 500+)
                         <br />I worked on agile scrum methodology and involving in stand-up meetings to monitor progress(Microsoft Azure, JIRA)</p>                    </div>
